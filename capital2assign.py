daa3d298d36403488e5c5ec7ea8e32b1596d0fc8
sudeeta <jainsudeeta@gmail.com>
2019-02-20 20:40:43 +0530

    adding function for summing

--- capital2assign.py
+++ capital2assign.py
@@ -4,3 +4,10 @@ listnum = [1,2,3,4]
 
 def square(num):
     return num * num
+
+def sum(numlist):
+    numsum = 0
+    for num in numlist:
+        numsum = numsum + num
+    else:
+        print(numsum)
