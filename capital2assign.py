8d577f3ed4806ef4be68b4a5fe8012b802c9b911
sudeeta <jainsudeeta@gmail.com>
2019-02-20 20:41:27 +0530

    adding multithreading code

--- capital2assign.py
+++ capital2assign.py
@@ -11,3 +11,16 @@ def sum(numlist):
         numsum = numsum + num
     else:
         print(numsum)
+
+with Pool(5) as p:
+        print("Result(process p1):",p.map(square,listnum ))
+        sum(p.map(square,listnum ))
+
+
+if __name__=="__main__":
+    listnum = [1,2,3,4]
+    
+
+    with Pool(5) as p:
+        print("Result(main program):",p.map(square,listnum ))
+        sum(p.map(square,listnum ))
