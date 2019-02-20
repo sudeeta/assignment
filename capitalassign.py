913094deaae9a5fb21bd1befecf37a6746ea5cbc
sudeeta <jainsudeeta@gmail.com>
2019-02-20 20:35:22 +0530

    adding class named_solution

--- capitalassign.py
+++ capitalassign.py
@@ -1,1 +1,10 @@
+from itertools import groupby
+class named_solution:
+    def groupAnagrams(ar):
+        a2=[list(group) for key,group in groupby(sorted(ar,key=sorted),sorted)]
+        a2.sort(reverse=True,key=len)
+        print("[",sep="")
+        for i in a2:
+            print("\n",i,)
 
+        print("\n]")
